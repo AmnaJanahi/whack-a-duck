693cf56ab9eb3134bd6bac8e85b1e4f2fa58a9ed
Amna Janahi <amna.m.janahi@gmail.com>
2025-07-09 13:33:13 +0300

    create play function

--- scripts/app.js
+++ scripts/app.js
@@ -2,16 +2,42 @@ function init(){
     // All code comes here
     const gridElem = document.querySelector('.grid')
 
+    const cells = []
+    const gridWidth = 10
+    const numberOfCells = gridWidth*gridWidth
+
+    let duckPosition = 20
+
+    function addDuck() {
+        cells[duckPosition].classList.add('duck')
+    }
+
+    function removeDuck() {
+        cells[duckPosition].classList.remove('duck')
+    } 
+
+     function play() {
+        setInterval(() => {
+            removeDuck()
+            duckPosition = Math.floor(Math.random() * numberOfCells)
+            addDuck()
+        }, 3000)
+    }
+    
     function createGrid(){
         // for every cell that we require create a div
         // append this cell to our grid
-        for (let i = 0; i <100; i++){
+        for (let i = 0; i <numberOfCells; i++){
             const cell = document.createElement('div')
+            // cell.classList.add('duck')
             cell.textContent = i
+            cells.push(cell)
             gridElem.appendChild(cell)
         }
+        console.log(cells)
     }
     createGrid()
+    play()
 }
 
 document.addEventListener('DOMContentLoaded', init)
